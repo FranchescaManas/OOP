eac2b96ad34277f93fe757e163983bb593567620
Franchesca Manas <franchesca.manas@gmail.com>
2023-09-02 09:15:31 +0800

    setting up repository

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title">
+        <br>
+        <label for="price">Price</label>
+        <input type="text" name="price" id="price">
+        <br>
+        <button type="submit" name="btn_submit">Submit</button>
+    </form>
+</body>
+</html>
